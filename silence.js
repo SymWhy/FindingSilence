dc22e1b53d7a1376faf95a9d43cc1a178793740a
SymWhy <simone.whysel@gmail.com>
2021-10-30 16:45:17 -0400

    create audio analyzer node

--- silence.js
+++ silence.js
@@ -9,24 +9,28 @@
     const playBtn = document.querySelector('button');
     const volSlider = document.querySelector('.volume');
 
+    const analyzer = audioCtx.createAnalyser();
+    analyzer.fftSize = 2048;
+
     const audioSource = audioCtx.createMediaElementSource(audioElement);
+
+    let isPlaying = false;
 
     //if the button is clicked, pause or play
     playBtn.addEventListener('click', function() {
-        if (audioCtx.state === 'suspended') {
-            audioCtx.resume();
-        }
         //if the audio is paused, play it
         if (this.getAttribute('class') === 'paused') {
         audioElement.play();
         this.setAttribute('class', 'playing');
         this.textContent = 'Pause';
+        isPlaying = true;
         }
         //if the audio is playing, pause it
         else if (this.getAttribute('class') === 'playing') {
             audioElement.pause();
             this.setAttribute('class', 'paused');
             this.textContent = 'Play';
+            isPlaying = false;
         }
     });
 
@@ -34,15 +38,28 @@
     audioElement.addEventListener('ended', function() {
         playBtn.setAttribute('class', 'paused');
         playBtn.textContent = 'Play';
+        isPlaying = false;
     });
 
     //gain node (volume)
     const gainNode = audioCtx.createGain();
 
     //listen for slider value change
-
     volSlider.addEventListener('input', function() {
         gainNode.gain.value = this.value;
     });
     
     audioSource.connect(gainNode).connect(audioCtx.destination);
+    audioSource.connect(analyzer);
+
+    let data = new Uint8Array(analyzer.frequencyBinCount);
+
+    function looper() {
+        requestAnimationFrame(looper);
+        analyzer.getByteFrequencyData;
+    }
+
+    audioElement.onplay = () => {
+        audioCtx.resume();
+        looper();
+    }
